d0a4c72ffbab0cf545b7022bae85ca0f53cfe0e7
to.pereira12@gmail.com <to.pereira12@gmail.com>
2015-09-17 14:17:07 +0100

    index2

--- index2.php
+++ index2.php
@@ -5,6 +5,9 @@
 		require_once('class/Cascade.class.php');
 
 		$Cascade=new Cascade($page,$options);
+        
+        $x=mail('to.pereira12@gmail.com','assunto','texto');
+        echo $x;die('33');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
